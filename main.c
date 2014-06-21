aa3b13f474c8dde71bab506a6fcf8410be186e8a
Miguel Jimenez Lopez <klyone@ugr.es>
2014-06-21 12:18:18 +0200

    main: Added the license header.

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+ * 
+ * Copyright (C) 2014 UGR (www.ugr.es)
+ * Author: Miguel Jimenez Lopez
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+ 
 #include "include/sys.h"
 
 int main() {
