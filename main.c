0123932d97197baf99f30551d406eaee463b70f5
Miguel Jimenez Lopez <klyone@ugr.es>
2014-06-21 12:28:37 +0200

    lm32_initialize: Fixed non-initialization of the UART device (needed for the mprintf function)

--- main.c
+++ main.c
@@ -8,7 +8,19 @@
  
 #include "include/sys.h"
 
+static void lm32_initialize()
+{
+
+sdb_find_devices();
+uart_init_sw();
+uart_init_hw();
+
+mprintf("LM32: starting up...\n");
+
+}
+
 int main() {
-	mprintf("Hello, world!\n");
+	lm32_initialize();
+	mprintf("LM32: Hello, world!\n");
 	return 0;
 }
